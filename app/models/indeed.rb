1cfc5ff5a84a9e493506eeefb735d00ed57d25d8
c1505 <coreym247@gmail.com>
2016-09-25 17:51:03 -0400

    updated sorting of results

--- app/models/indeed.rb
+++ app/models/indeed.rb
@@ -6,11 +6,11 @@ class Indeed
   end
 
   def fetch_one
-    response = HTTParty.get("http://api.indeed.com/ads/apisearch?publisher=#{@publisher_key}&format=json&q=#{@query}&l=#{@zip_code}%2C&sort=&radius=&st=&jt=&start=&limit=50&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2")
+    response = HTTParty.get("http://api.indeed.com/ads/apisearch?publisher=#{@publisher_key}&format=json&q=#{@query}&l=#{@zip_code}%2C&sort=date&radius=&st=&jt=&start=&limit=50&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2")
   end
 
   def fetch(num)
-    response = HTTParty.get("http://api.indeed.com/ads/apisearch?publisher=#{@publisher_key}&format=json&q=#{@query}&l=#{@zip_code}%2C&sort=&radius=&st=&jt=&start=#{num}&limit=50&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2")
+    response = HTTParty.get("http://api.indeed.com/ads/apisearch?publisher=#{@publisher_key}&format=json&q=#{@query}&l=#{@zip_code}%2C&sort=date&radius=&st=&jt=&start=#{num}&limit=50&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2")
   end
 
   def fetch_all
